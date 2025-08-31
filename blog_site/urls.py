ac9ab5c196e495ea6486ba624e9f8285eb45a680
Luke <Ni099100077@outlook.com>
2025-08-31 13:44:45 +0800

    添加数据初始化管理命令

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
-    path('', include('blog.urls', namespace='blog')),  # 首页也显示博客
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('', blog_views.post_list, name='home'),  # 首页直接显示博客列表
 ]
